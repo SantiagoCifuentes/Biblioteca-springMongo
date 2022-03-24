5d678c3f351519ec2b3979fd3aabd4bee55631bb
Santiago C <santiago.ci9619@gmail.com>
2022-03-23 23:28:16 -0500

    Initial commit

---
+++ src/main/java/co/com/sofka/biblioteca/reactiva/Blbioteca/Reactiva/BlbiotecaReactivaApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.biblioteca.reactiva.Blbioteca.Reactiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlbiotecaReactivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlbiotecaReactivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/sofka/biblioteca/reactiva/Blbioteca/Reactiva/BlbiotecaReactivaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.biblioteca.reactiva.Blbioteca.Reactiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlbiotecaReactivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
